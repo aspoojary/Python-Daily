9298bca80b635dcec796f8556508d4e36d60a02b
aspoojary <shankarap200@gmail.com>
2026-05-03 12:22:41 +0200

    conca

---
+++ Str_concant.py
@@ -0,0 +1,13 @@
+#🟢 Exercise 1: Basic Concatenation
+
+"""Create two variables:
+
+first_name
+last_name
+
+Concatenate them into a full name with a space in between and print it."""
+
+A= "Jhon"
+B = "Doe"
+
+print(A+ " "+ B)
