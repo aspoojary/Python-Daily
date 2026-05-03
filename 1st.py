654b46ce99e3dab610167474adf8690fd4b83ba6
aspoojary <shankarap200@gmail.com>
2026-05-03 09:58:03 +0200

    just changed the code

--- 1st.py
+++ 1st.py
@@ -1,1 +1,10 @@
+#🟢 Exercise 1: Basic Concatenation
+
+"""Create two variables:
+
+first_name
+last_name
+
+Concatenate them into a full name with a space in between and print it."""
+
 print("Hello")
